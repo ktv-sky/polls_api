33e07d6f659a7a1fc6cda99e7e3db1321701fb84
KTV <helicooper7@gmail.com>
2021-01-28 20:26:05 +0300

    Add api views

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import get_object_or_404, render, get_list_or_404
-from django.http import JsonResponse
+from rest_framework import generics
 
-from .models import Poll
-
-def polls_list(request):
-    polls = Poll.objects.all()[:20]
-    data = {'results': list(polls.values(
-        'question',
-        'created_by__username',
-        'pub_date'
-    ))}
-    return JsonResponse(data)
+from .models import Poll, Choice
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
 
-def polls_detail(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-    data = {'results': {
-        'question': poll.question,
-        'created_by': poll.created_by.username,
-        'pub_date': poll.pub_date
-    }}
-    return JsonResponse(data)
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
